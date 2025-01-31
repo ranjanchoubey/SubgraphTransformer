9579684e79ee8a3bc48b1c6e62c925b64c366237
Your Name <you@example.com>
2025-01-31 18:11:17 +0530

    important

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -10,30 +10,7 @@ def load_cora_data():
     print(f"Dataset Loaded: Cora, Number of nodes: {graph.number_of_nodes()}")
     return graph
 
-def partition_graph(graph, num_parts=4):
-    """
-    Partitions the graph into subgraphs using DGL's METIS implementation.
-    Args:
-        graph: DGL graph object
-        num_parts: Number of partitions
-    Returns:
-        list of subgraphs
-    """
-    # Get partition assignments for each node
-    partition_labels = dgl.metis_partition_assignment(graph, num_parts)
-    
-    # Add partition labels as node features
-    graph.ndata['part_id'] = partition_labels
-    
-    # Create subgraphs
-    subgraphs = []
-    for part_id in range(num_parts):
-        node_mask = graph.ndata['part_id'] == part_id
-        subgraph = dgl.node_subgraph(graph, node_mask)
-        subgraphs.append(subgraph)
-    
-    print(f"Graph partitioned into {num_parts} subgraphs.")
-    return subgraphs
+
 
 def test_embeddings(graph, subgraphs):
     """
