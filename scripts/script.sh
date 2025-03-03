ce9e265556e60a231276f6988bc7e8df351f0e06
Your Name <you@example.com>
2025-03-03 12:23:15 +0530

    requirements file updated , added instruction to run on any server

--- scripts/script.sh
+++ scripts/script.sh
@@ -11,7 +11,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 # install dependencies
-pip install -e ".[dev]"
+pip install -r requirements.txt
 
 # Run main.py file (entry point of code small dataset)
 python main.py  --dataset CoraSmall   --config 'configs/cora_small.json' --gpu_id 0
